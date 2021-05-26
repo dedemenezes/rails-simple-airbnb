6f557f2061dcf0ffb16b18ad1eedda5c1bbbd026
Andre Menezes <djdedemenezes@gmail.com>
2021-05-26 20:11:31 -0300

    #New done

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,7 +4,22 @@ class FlatsController < ApplicationController
     @flats = Flat.all
   end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    raise
+    @flat = Flat.new(flat_params)
+  end
+
   def show
     @flat = Flat.find(params[:id])
   end
+
+  private
+
+  # def flat_params
+  #   params.require(:)
+  # end
 end
